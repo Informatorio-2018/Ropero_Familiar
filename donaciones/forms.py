6a1baf602240b7fb5a68b2185f6e7f7f91b6feea
Facundo <c.facu98@gmail.com>
2018-11-13 17:58:06 -0300

    Hice el registro del referente

--- donaciones/forms.py
+++ donaciones/forms.py
@@ -1,12 +1,23 @@
 from django import forms
-from .models import Family, Donation
+from .models import Family, Donation, Referring
+
+class FamilyForm_r(forms.ModelForm):
+	class Meta:
+		model = Family
+		fields = ('firstname','lastname','dni','birth')
 
 class FamilyForm(forms.ModelForm):
 	class Meta:
 		model = Family
-		fields = '__all__'
+		fields = ('firstname','lastname','dni','birth')
 
 class DonationForm(forms.ModelForm):
     class Meta:
         model = Donation
         fields = '__all__'
+
+class ReferringForm(forms.ModelForm):
+	class Meta:
+		model = Referring
+		fields = ('neighborhood','phone','adress')
+
