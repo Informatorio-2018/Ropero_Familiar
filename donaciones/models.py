440d77b74b954fea1aa1e239b8aeed21c882addc
CristianJova <cristian.jova149@gmail.com>
2018-11-20 16:32:52 -0300

    terminado agregar donaciones

--- donaciones/models.py
+++ donaciones/models.py
@@ -7,24 +7,27 @@ UNITS_MEASURE = (('kg', 'Kg'), ('par', 'Par'), ('un', 'Unidad'), ('lt', 'Litro')
 
 class Donation(models.Model):
     date = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=80)
-    ticket_number = models.IntegerField(null=True)
+    name = models.CharField(max_length=80, verbose_name='Nombre o Razón Social')
+    ticket_number = models.IntegerField(null=True, verbose_name='Numero de Recibo')
 
     def __str__(self):
         return self.name
 
 
 class DetailsDonation(models.Model):
-    donation_type = models.CharField(max_length=30)
-    unit_measure = models.CharField(max_length=10, choices=UNITS_MEASURE)
-    quantity = models.IntegerField()
+    donation_type = models.CharField(max_length=30, verbose_name='Tipo de Donación')
+    unit_measure = models.CharField(max_length=10, choices=UNITS_MEASURE, verbose_name='Unidad de Medida')
+    quantity = models.IntegerField(verbose_name='Cantidad')
     donation = models.ForeignKey(Donation, on_delete=models.CASCADE, related_name='detailsdonation')
+
+    def __str__(self):
+        return self.donation_type
 
 
 class TypesDonation(models.Model):
-    name = models.CharField(max_length=30)
-    unit_measure = models.CharField(max_length=10, choices=UNITS_MEASURE)
-    quantity_total = models.IntegerField(default=0)
+    name = models.CharField(max_length=30, verbose_name='Nombre')
+    unit_measure = models.CharField(max_length=10, choices=UNITS_MEASURE, verbose_name='Unidad de Medida')
+    quantity_total = models.IntegerField(default=0, verbose_name='Cantidad Total')
 
     def __str__(self):
         return self.name
@@ -35,16 +38,17 @@ class Neighborhood(models.Model):
 
 
 class Family(models.Model):
-	lastname = models.CharField(max_length=30)
-	firstname = models.CharField(max_length=30)
-	dni = models.PositiveIntegerField()#Limitar numeros
-	birth = models.DateField(null=True)
-	role = models.CharField(max_length=1,choices=(('r','Referente'),('f','Familiar')))
+    lastname = models.CharField(max_length=30)
+    firstname = models.CharField(max_length=30)
+    dni = models.PositiveIntegerField()  # Limitar numeros
+    birth = models.DateField(null=True)
+    role = models.CharField(max_length=1, choices=(('r', 'Referente'), ('f', 'Familiar')))
+
 
 class Referring(models.Model):
-	neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='referentes', null=True)
-	phone = models.PositiveIntegerField()#Limitar numero de telefono
-	adress = models.CharField(max_length=80)
-	family = models.OneToOneField(Family, on_delete=models.CASCADE)
-	last_buy = models.DateTimeField(null=True)
-	family_last_buy = models.CharField(max_length=65,null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='referentes', null=True)
+    phone = models.PositiveIntegerField()  # Limitar numero de telefono
+    adress = models.CharField(max_length=80)
+    family = models.OneToOneField(Family, on_delete=models.CASCADE)
+    last_buy = models.DateTimeField(null=True)
+    family_last_buy = models.CharField(max_length=65, null=True)
--- donaciones/views.py
+++ donaciones/views.py
@@ -19,6 +19,8 @@ def home(request):
     return render(request, 'home.html', {})
 
 # Cristian
+
+
 def receive_donation(request):
     form = DonationForm(request.POST or None)
     if request.method == 'POST':
@@ -33,28 +35,61 @@ def receive_donation(request):
 def items_donation(request, id):
     donator = Donation.objects.get(pk=id)
     types = TypesDonation.objects.all()
-    context = {'donator': donator, 'types': types}
+    if request.method == 'POST':
+        form = DetailsDonationForm(request.POST)
+        if form.is_valid():
+            detail = form.save(commit=False)
+            detail.donation_type = request.POST['donation_type']
+            detail.unit_measure = request.POST['unit_measure']
+            detail.donation_id = donator.id
+            detail.save()
+    else:
+        form = DetailsDonationForm()
+    context = {'donator': donator, 'types': types, 'form': form}
     return render(request, 'items_donation.html', context)
 
-# Facu
-def register_referring_f(request):
-	form = FamilyForm_r(request.POST or None)
-	if request.method == 'POST':
-		if form.is_valid():
-			ref = form.save(commit=False)
-			ref.role = 'r'
-			ref.save()
-			family = Family.objects.last()
-			return redirect('register_referring',id=family.id)
-	return render(request,'register_referring.html',{'form':form})
 
-def register_referring(request,id):
-	form = ReferringForm(request.POST or None)
-	family = Family.objects.get(pk=id)
-	if request.method == 'POST':
-		if form.is_valid():
-			fam = form.save(commit=False)
-			fam.family = family
-			form.save()
-			return redirect('home')
-	return render(request,'register_referring.html',{'form':form})
+def resume_donation(request, id):
+    donator = Donation.objects.get(pk=id)
+    context = {'donator': donator, 'resumes': DetailsDonation.objects.filter(donation__pk=id)}
+    return render(request, 'resume_donation.html', context)
+
+
+def finish_donation(request, id):
+    donator = Donation.objects.get(pk=id)
+    if request.method == 'POST':
+        form = TicketForm(request.POST, instance=donator)
+        if form.is_valid():
+            donator.ticket_number = request.POST['ticket_number']
+            donator.save()
+            return redirect('receive_donation')
+    else:
+        form = TicketForm()
+    context = {'donator': donator, 'form': form}
+    return render(request, 'finish_donation.html', context)
+
+# Facu
+
+
+def register_referring_f(request):
+    form = FamilyForm_r(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            ref = form.save(commit=False)
+            ref.role = 'r'
+            ref.save()
+            family = Family.objects.last()
+            return redirect('register_referring', id=family.id)
+    return render(request, 'register_referring.html', {'form': form})
+
+
+def register_referring(request, id):
+    form = ReferringForm(request.POST or None)
+    family = Family.objects.get(pk=id)
+    if request.method == 'POST':
+        if form.is_valid():
+            fam = form.save(commit=False)
+            fam.family = family
+            form.save()
+            return redirect('home')
+    return render(request, 'register_referring.html', {'form': form})
