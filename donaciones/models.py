41ef9a84c3fe219fff6fb21c13bd5770180f0c5b
Facundo <c.facu98@gmail.com>
2018-11-19 22:25:21 -0300

    Se agrego el datepicker y dropdown de los barrios

--- donaciones/models.py
+++ donaciones/models.py
@@ -40,17 +40,17 @@ class Neighborhood(models.Model):
         return "%s" % (self.name)
 
 class Family(models.Model):
-    lastname = models.CharField(max_length=30)
-    firstname = models.CharField(max_length=30)
-    dni = models.PositiveIntegerField()  # Limitar numeros
-    birth = models.DateField(null=True)
+    lastname = models.CharField(max_length=30,verbose_name='Nombre')
+    firstname = models.CharField(max_length=30,verbose_name='Apellido')
+    dni = models.PositiveIntegerField(validators=[MaxValueValidator(99999999)],verbose_name='DNI')  # Limitar numeros
+    birth = models.DateField(null=True,verbose_name='Fecha de Nacimiento')
     role = models.CharField(max_length=1, choices=(('r', 'Referente'), ('f', 'Familiar')))
 
 
 class Referring(models.Model):
-    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='referentes', null=True)
-    phone = models.PositiveIntegerField()  # Limitar numero de telefono
-    adress = models.CharField(max_length=80)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='referentes', null=True, verbose_name='Barrio')
+    phone = models.PositiveIntegerField(verbose_name='Número de Teléfono')  # Limitar numero de telefono
+    adress = models.CharField(max_length=80,verbose_name='Dirección')
     family = models.OneToOneField(Family, on_delete=models.CASCADE)
     last_buy = models.DateTimeField(null=True)
     family_last_buy = models.CharField(max_length=65, null=True)
