1c847e79792f4ec6c898b71d847eac0c60e54061
Facundo <c.facu98@gmail.com>
2018-12-03 19:27:52 -0300

    Agregue botones al menu, arregle un reporte de donaciones

--- donaciones/forms.py
+++ donaciones/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+import datetime
 
 
 class FamilyForm_r(forms.ModelForm):
